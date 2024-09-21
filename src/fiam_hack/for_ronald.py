412e53c00f5e519e98772d5974b3d1acd6c0d03e
Ronald <ron.play10@gmail.com>
2024-09-21 11:28:52 -0400

    tensorflow update

--- src/fiam_hack/for_ronald.py
+++ src/fiam_hack/for_ronald.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Embedding, LSTM, Dense, Dropout, Concatenate
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.python.keras.layers import Input, Embedding, LSTM, Dense, Dropout, Concatenate
+from tensorflow.python.keras.optimizers import Adam
+from tensorflow.python.keras.callbacks import EarlyStopping
+
 
 print("hello world")
