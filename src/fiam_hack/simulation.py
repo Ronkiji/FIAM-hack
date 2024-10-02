7dcd0bd76f19968a7cc44e1d27476cb42d2d5b64
angelachenn <angela.c1331@gmail.com>
2024-10-02 15:16:29 -0400

    optimization and simulation

--- src/fiam_hack/simulation.py
+++ src/fiam_hack/simulation.py
@@ -4,16 +4,18 @@ import matplotlib.pyplot as plt
 
 
 # Change as needed
-start_date = '2012-01-01'
-end_date = '2012-11-01'
+start_date = '2010-01-01'
+end_date = '2023-12-01'
 
 optimal_weights = pd.read_csv('optimal_weights.csv')
 optimal_weights = optimal_weights.loc[:, ['date', 'permno', 'weight']]
 optimal_weights['date'] = pd.to_datetime(optimal_weights['date'])
+optimal_weights.sort_values('date', inplace=True)
 
 actual_returns = pd.read_csv('hackathon_sample_v2.csv')
 actual_returns = actual_returns.loc[:, ['year','month','permno', 'stock_exret']]
 actual_returns['date'] = pd.to_datetime(actual_returns[['year', 'month']].assign(day=1))
+actual_returns.sort_values('date', inplace=True)
 
 portfolio_value = 100
 cumulative_values = []
@@ -28,7 +30,6 @@ for year_month in unique_year_months:
 
     # Merge weights and actual returns for the same permnos (stocks) based on year-month
     merged_data = pd.merge(weights, returns, on=['permno', 'date'])
-    # print(merged_data)
     # Calculate the portfolio return for this month
     monthly_return = np.sum(merged_data['weight'] * merged_data['stock_exret'])
     
@@ -44,7 +45,6 @@ for year_month in unique_year_months:
 
 cumulative_df = pd.DataFrame(cumulative_values)
 cumulative_df.to_csv('cumulative_portfolio_values.csv', index=False)
-print(cumulative_df)
 
 # Graphing prep
 market_data = pd.read_csv('mkt_ind.csv')
@@ -53,12 +53,13 @@ market_data.sort_values('date', inplace=True)
 plotting_data = pd.merge(cumulative_df, market_data[['date', 'rf', 'sp_ret']], on='date', how='inner')
 plotting_data = plotting_data[(plotting_data['date'] >= start_date) & (plotting_data['date'] <= end_date)]
 
-# Calculate cumulative returns for the S&P 500
+# Calculate cumulative returns for the S&P 500 and our trading strategy
+plotting_data['cumulative_portfolio'] = (1 + plotting_data['monthly_return']).cumprod()
 plotting_data['cumulative_sp500'] = (1 + plotting_data['sp_ret']).cumprod()
 
 # Plot the cumulative returns
 plt.figure(figsize=(10, 6))
-plt.plot(plotting_data['date'], plotting_data['monthly_return'], label='Trading Strategy', color='blue')
+plt.plot(plotting_data['date'], plotting_data['cumulative_portfolio'], label='Trading Strategy', color='blue')
 plt.plot(plotting_data['date'], plotting_data['cumulative_sp500'], label='S&P 500', color='red')
 plt.title('Cumulative Performance of Trading Strategy vs. S&P 500 ' + str(start_date) + ' - ' + str(end_date))
 plt.xlabel('Date')
