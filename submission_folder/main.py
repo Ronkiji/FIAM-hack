23324d5229182bc2df071454378237edcf356799
Ronald <ron.play10@gmail.com>
2024-10-03 16:10:10 -0400

    reformatted main.py

--- submission_folder/main.py
+++ submission_folder/main.py
@@ -2,12 +2,16 @@ import pandas as pd
 import lstm
 import optimization
 
-df = pd.read_csv("hackathon_sample_v2.csv")
+# df = pd.read_csv("hackathon_sample_v2.csv")
+df = pd.read_csv("testing.csv")
 
 # run the model - all the variables are at the top of the file
-# results = lstm.run(df)
+results = lstm.run(df.copy())
 
-test_df = pd.read_csv("final_output_20241001_202812.csv")
+# if you don't want to run the model
+# uncomment line 13, and comment out line 9
+# results = pd.read_csv("results.csv")
+
 weights = optimization.run(results, df)
 
 # simulate(weights)
